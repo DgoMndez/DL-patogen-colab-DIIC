a9f490ddceef35243cc7dcb8d2b3d4750927a6d4
DgoMndez <dgo.mendezg@gmail.com>
2024-01-05 18:46:28 +0100

    def clean_abstract (-puntos, stopwords)

--- src/fine-tuning/trainingdata.py
+++ src/fine-tuning/trainingdata.py
@@ -6,11 +6,38 @@ import os
 import logging
 from sklearn.model_selection import train_test_split
 
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# Download the stopwords from NLTK
+#nltk.download('punkt')
+#nltk.download('stopwords')
+
+def clean_abstract(abstract):
+    # Convert the text to lowercase
+    abstract = abstract.lower()
+
+    # Remove punctuation
+    abstract = abstract.translate(str.maketrans('', '', string.punctuation))
+
+    # Tokenize the text
+    tokens = word_tokenize(abstract)
+
+    # Remove stopwords
+    tokens = [word for word in tokens if not word in stopwords.words()]
+
+    # Join the tokens back into a single string
+    abstract = ' '.join(tokens)
+
+    return abstract
+
 PATH_DATA = '../pubmed-queries/abstracts'
 PATH_DATA_CSV = PATH_DATA + '/abstracts.csv'
 PATH_DATA_FENOTIPOS = '../pubmed-queries/results/phenotypes-22-12-15.csv'
 SEED = 42
-VERBOSE_COMP = True
+VERBOSE_COMP = False
 if __name__ == '__main__':
     
     #TODO:
@@ -37,5 +64,12 @@ if __name__ == '__main__':
     dfPhenotypes = pd.read_csv(PATH_DATA_FENOTIPOS, sep=';', low_memory=False, na_values=[''])
     # Assuming dfPapers is your DataFrame
     train_df, test_df = train_test_split(dfPapers, test_size=0.2, random_state=42)
+
+    df = dfPapers.sample(10, random_state=SEED)
+    for index, row in df.iterrows():
+        abstract = row['abstract']
+        abstract = clean_abstract(abstract)
+        print(abstract)
+        # Fine-tune your BERT transformer with the cleaned abstract
 
 
